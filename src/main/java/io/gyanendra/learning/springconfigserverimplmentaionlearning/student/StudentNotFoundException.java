20f8bf57a0b637f3a7654a257aa86aef59cd597d
DESKTOP-FRL0M1V\Gyanendra-Yadav <gyanendra.yadav@deltatech.gg>
2022-11-08 14:06:48 +0530

    Adding file in the codebase.

---
+++ src/main/java/io/gyanendra/learning/springconfigserverimplmentaionlearning/student/StudentNotFoundException.java
@@ -0,0 +1,12 @@
+package io.gyanendra.learning.springconfigserverimplmentaionlearning.student;
+
+/**
+ * Created By: Gyanendra_Yadav on 2022-11-08,Nov,2022
+ */
+public class StudentNotFoundException extends RuntimeException {
+
+    public StudentNotFoundException(String exception) {
+        super(exception);
+    }
+
+}
